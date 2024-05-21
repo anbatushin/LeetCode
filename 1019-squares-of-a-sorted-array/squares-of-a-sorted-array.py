b4a6cf4d740095bed86906e9c870842226cd1a55
anbatushin <114628285+anbatushin@users.noreply.github.com>
2024-05-21 15:35:31 +0300

    Time: 153 ms (66.46%) | Memory: 18.6 MB (85.98%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,25 +1,5 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        def quicksort(nums, fst, lst):
-            if fst >= lst:
-                return
-            left, right = fst, lst
-            pivot = nums[(fst + lst) // 2]
-
-            while left <= right:
-                while nums[left] < pivot:
-                    left += 1
-                while nums[right] > pivot:
-                    right -= 1
-                if left <= right:
-                    nums[left], nums[right] = nums[right], nums[left]
-                    left += 1
-                    right -= 1
-
-            quicksort(nums, fst, right)
-            quicksort(nums, left, lst)
-        
         for i in range(len(nums)):
             nums[i] *= nums[i]
-        quicksort(nums, 0, len(nums) - 1)
-        return nums
+        return sorted(nums)
